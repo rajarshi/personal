9d1eb2f9e61a4d06972c07ccae60759464cfce3f
Rajarshi Guha <rguha@localhost.localdomain>
2009-02-05 11:57:05 -0500

    Added build script to get a sinlge jar file for all rest services. Updated application to take command line args

--- CDKPersonal/src/net/guha/apps/rest/CDKServices.java
+++ CDKPersonal/src/net/guha/apps/rest/CDKServices.java
@@ -3,11 +3,19 @@ package net.guha.apps.rest;
 import net.guha.apps.rest.resources.DepictionResource;
 import net.guha.apps.rest.resources.SubstructureSearchResource;
 import net.guha.apps.rest.resources.TPSAResource;
+import org.apache.commons.cli.*;
 import org.restlet.*;
 import org.restlet.data.Protocol;
+import org.restlet.service.LogService;
+
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
 
 
 public class CDKServices extends Application {
+
+    private static Logger logger = Logger.getLogger("net.guha");
+
     public synchronized Restlet createRoot() {
         Router router = new Router(getContext());
 
@@ -23,10 +31,53 @@ public class CDKServices extends Application {
     }
 
     public static void main(String[] args) throws Exception {
+        Options options = new Options();
+        options.addOption("h", "help", false, "Print this message");
+        options.addOption("s", "server", true, "Host name for the web server. Default is localhost");
+        options.addOption("p", "port", true, "Port number. Default is 8182");
+        options.addOption("l", "log", true, "Name for log file. If not specified " +
+                " log messages are sent to STDOUT");
+
+        CommandLine line = null;
+        try {
+            CommandLineParser parser = new PosixParser();
+            line = parser.parse(options, args);
+        } catch (ParseException exception) {
+            System.err.println("Unexpected exception: " + exception.toString());
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("\njava -jar cdkrest.jar",
+                    "CDK REST services (Rajarshi Guha <rajarshi.guha@gmail.com>)\n",
+                    options, "");
+            System.exit(-1);
+        }
+        if (line.hasOption("h")) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("java -jar cdkrest.jar [OPTIONS]", options);
+            System.exit(-1);
+        }
+        String server = "localhost";
+        int port = 8182;
+        String logfilename = null;
+
+        if (line.hasOption("s")) server = line.getOptionValue("s");
+        if (line.hasOption("p")) port = Integer.parseInt(line.getOptionValue("p"));
+        if (line.hasOption("l")) logfilename = line.getOptionValue("l");
+
         Component component = new Component();
-        component.getServers().add(Protocol.HTTP, "rguha.ath.cx", 8182);
+        component.getServers().add(Protocol.HTTP, server, port);
         component.getDefaultHost().attach(new CDKServices());
-//        component.getDefaultHost().attach("/trace/{user}", new TracerRestlet());
+
+
+        LogService logService = component.getLogService();
+        logService.setEnabled(true);
+        logService.setLoggerName("net.guha");
+
+        if (logfilename != null) {
+            FileHandler fh = new FileHandler(logfilename);
+            logger.setUseParentHandlers(false);
+            logger.addHandler(fh);
+        }
+
         component.start();
     }
 }
