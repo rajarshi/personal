b579a44402960ac643cd7364db0cb406f86443cf
Rajarshi Guha <rguha@localhost.localdomain>
2009-02-10 17:20:49 -0500

    Added a class to display multiple structures at one go

---
+++ CDKPersonal/src/net/guha/util/cdk/MultiStructurePanel.java
@@ -0,0 +1,61 @@
+package net.guha.util.cdk;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class MultiStructurePanel extends JFrame {
+    public MultiStructurePanel(Renderer2DPanel[] panels,
+                               int ncol, int cellx, int celly) throws HeadlessException {
+                int pad = 5;
+
+        int extra = panels.length % ncol;
+        int block = panels.length - extra;
+        int nrow = block / ncol;
+
+        int i;
+        int j;
+
+        JFrame frame = new JFrame("Molecule Fragmenter");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        JPanel spane = new JPanel(new GridBagLayout(), true);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        //gbc.insets = new Insets(2, 2, 2, 2);
+
+        int cnt = 0;
+        for (i = 0; i < nrow; i++) {
+            for (j = 0; j < ncol; j++) {
+                gbc.gridx = j;
+                gbc.gridy = i;
+                spane.add(panels[cnt], gbc);
+                cnt += 1;
+            }
+        }
+        j = 0;
+        while (cnt < panels.length) {
+            gbc.gridy = nrow;
+            gbc.gridx = j;
+            spane.add(panels[cnt], gbc);
+            cnt += 1;
+            j += 1;
+        }
+
+        if (extra != 0)
+            nrow += 1;
+
+        JScrollPane scrollpane = new JScrollPane(spane,
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        frame.getContentPane().add(scrollpane);
+
+        // start the show!
+        frame.pack();
+        if (nrow > 3) {
+            frame.setSize(ncol * cellx + pad, 3 * celly + pad);
+        } else {
+            frame.setSize(ncol * cellx + pad, nrow * celly + pad);
+        }
+    }
+}
