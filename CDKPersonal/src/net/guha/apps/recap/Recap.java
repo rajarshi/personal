48f27dded25fab10dfa3d500246d0542b2810171
Rajarshi  Guha <rajarshi.guha@gmail.com>
2009-02-10 20:08:28 -0500

    Added recap rule 3

--- CDKPersonal/src/net/guha/apps/recap/Recap.java
+++ CDKPersonal/src/net/guha/apps/recap/Recap.java
@@ -39,7 +39,7 @@ public class Recap {
         arf.findAllRings(atomContainer);
 
         List<IAtomContainer> frags = new ArrayList<IAtomContainer>();
-        frags.addAll(recapRule02(atomContainer));
+        frags.addAll(recapRule03(atomContainer));
         return frags;
     }
 
@@ -66,11 +66,10 @@ public class Recap {
     }
 
     private List<IAtomContainer> recapRule02(IAtomContainer atomContainer) throws CDKException {
-//        sqt.setSmarts("[OD2][CD3]=O");
         sqt.setSmarts("[OD2][$([CD3]=O)]");
         if (!sqt.matches(atomContainer)) return null;
         List<List<Integer>> matches = sqt.getUniqueMatchingAtoms();
-        System.out.println("rule 1 : " + matches.size());
+        System.out.println("rule 2 : " + matches.size());
         List<IAtomContainer> ret = new ArrayList<IAtomContainer>();
         for (List<Integer> path : matches) {
             IAtom left = atomContainer.getAtom(path.get(0));
@@ -84,15 +83,33 @@ public class Recap {
         return ret;
     }
 
-       private List<IAtomContainer> recapRule06(IAtomContainer atomContainer) throws CDKException {
+    private List<IAtomContainer> recapRule03(IAtomContainer atomContainer) throws CDKException {
+        sqt.setSmarts("[ND3]-*");
+        if (!sqt.matches(atomContainer)) return null;
+        List<List<Integer>> matches = sqt.getUniqueMatchingAtoms();
+        System.out.println("rule 3 : " + matches.size());
+        List<IAtomContainer> ret = new ArrayList<IAtomContainer>();
+        for (List<Integer> path : matches) {
+            IAtom left = atomContainer.getAtom(path.get(0));
+            IAtom right = atomContainer.getAtom(path.get(1));
+            IBond splitBond = atomContainer.getBond(left, right);
+            if (splitBond.getFlag(CDKConstants.ISINRING)) continue;
+            IAtomContainer[] parts = splitMolecule(atomContainer, splitBond);
+            ret.add(parts[0]);
+            ret.add(parts[1]);
+        }
+        return ret;
+    }
+
+    private List<IAtomContainer> recapRule06(IAtomContainer atomContainer) throws CDKException {
         sqt.setSmarts("C=C");
         if (!sqt.matches(atomContainer)) return null;
         List<List<Integer>> matches = sqt.getUniqueMatchingAtoms();
-        System.out.println("rule 1 : " + matches.size());
+        System.out.println("rule 6 : " + matches.size());
         List<IAtomContainer> ret = new ArrayList<IAtomContainer>();
         for (List<Integer> path : matches) {
-            IAtom left =atomContainer.getAtom(path.get(0));
-            IAtom right =atomContainer.getAtom(path.get(1));
+            IAtom left = atomContainer.getAtom(path.get(0));
+            IAtom right = atomContainer.getAtom(path.get(1));
             IBond splitBond = atomContainer.getBond(left, right);
             if (splitBond.getFlag(CDKConstants.ISINRING)) continue;
             IAtomContainer[] parts = splitMolecule(atomContainer, splitBond);
@@ -192,17 +209,18 @@ public class Recap {
 //        String smiles = "N(C)(C)C(=O)CCCC(=O)N(N)C";
 //        String smiles = "CCN(C)CN";
 //        String smiles = "CNSCNN";
-        String smiles = "CC(=O)OC";
+//        String smiles = "CC(=O)OC";
 //        String smiles = "CC=CCC=CN";
+        String smiles = "N(C)(C)CCCC";
+
         IMolecule mol = sp.parseSmiles(smiles);
-        IBond breakBond = mol.getBond(3);
         AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(mol);
 
         List<IAtomContainer> f = recap.fragment(mol);
         System.out.println("f.size() = " + f.size());
 
         Renderer2DPanel[] panels = new Renderer2DPanel[f.size() + 1];
-        panels[0] = new Renderer2DPanel(Misc.get2DCoords(mol), recap.getNeedle(mol, breakBond), 200, 200, false, null, 0);
+        panels[0] = new Renderer2DPanel(Misc.get2DCoords(mol), 200,200);
         panels[0].setBorder(BorderFactory.createEtchedBorder(
                 EtchedBorder.LOWERED, Color.red, Color.gray));
 
