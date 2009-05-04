2a9091b316bb28ee8ece215b17f459c46773c963
Rajarshi  Guha <rajarshi.guha@gmail.com>
2009-05-04 01:32:29 -0400

    Simple MR to count atoms in molecules in SD format

---
+++ CDKPersonal/rghadoop/src/net/rguha/dc/SDFAtomCount.java
@@ -0,0 +1,83 @@
+package net.rguha.dc;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.openscience.cdk.ChemFile;
+import org.openscience.cdk.ChemObject;
+import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.io.MDLV2000Reader;
+import org.openscience.cdk.smiles.SmilesParser;
+import org.openscience.cdk.tools.manipulator.ChemFileManipulator;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
+
+public class SDFAtomCount {
+    static SmilesParser sp = new SmilesParser(DefaultChemObjectBuilder.getInstance());
+
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text haCount = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            try {
+                StringReader sreader = new StringReader(value.toString());
+                MDLV2000Reader reader = new MDLV2000Reader(sreader);
+                ChemFile chemFile = (ChemFile)reader.read((ChemObject)new ChemFile());
+                List<IAtomContainer> containersList = ChemFileManipulator.getAllAtomContainers(chemFile);
+                IAtomContainer molecule = containersList.get(0);
+
+                haCount.set(String.valueOf(molecule.getAtomCount()));
+                context.write(haCount, one);
+            } catch (CDKException e) {
+                // do nothing for now
+            }
+        }
+    }
+
+    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 2) {
+            System.err.println("Usage: countha <in> <out>");
+            System.exit(2);
+        }
+        Job job = new Job(conf, "haCount count");
+        job.setJarByClass(SDFAtomCount.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setInputFormatClass(SDFInputFormat.class);
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
