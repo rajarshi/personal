07e566be9c04bb61ee94590404ce9f16a4deda8b
Rajarshi  Guha <rajarshi.guha@gmail.com>
2009-05-11 00:34:55 -0400

    Got rid of a trivial example program. Updated pcore search program for 0.18.3

--- CDKPersonal/rghadoop/src/net/rguha/dc/PSearch.java
+++ CDKPersonal/rghadoop/src/net/rguha/dc/PSearch.java
@@ -1,19 +1,54 @@
 package net.rguha.dc;
 
+import net.rguha.dc.io.SDFInputFormat;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.openscience.cdk.CDKConstants;
+import org.openscience.cdk.ChemFile;
+import org.openscience.cdk.ChemObject;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.io.MDLV2000Reader;
+import org.openscience.cdk.pharmacophore.PharmacophoreMatcher;
+import org.openscience.cdk.pharmacophore.PharmacophoreQuery;
+import org.openscience.cdk.pharmacophore.PharmacophoreUtils;
+import org.openscience.cdk.tools.manipulator.ChemFileManipulator;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringBufferInputStream;
+import java.io.StringReader;
+import java.util.Iterator;
+import java.util.List;
+
 public class PSearch {
 
-    /*
     private static PharmacophoreMatcher pmatcher = new PharmacophoreMatcher();
 
     private final static IntWritable one = new IntWritable(1);
     private final static IntWritable zero = new IntWritable(0);
 
-    public static class MoleculeMapper extends Mapper<Object, Text, Text, IntWritable> {
+    public static class MoleculeMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
 
         private Text matches = new Text();
 
-        public void setup(Context context) {
-            String pattern = context.getConfiguration().get("net.rguha.dc.data.pcoredef");
+        @Override
+        public void configure(JobConf configuration) {
+            String pattern = configuration.get("net.rguha.dc.data.pcoredef");
             try {
                 StringBufferInputStream sbis = new StringBufferInputStream(pattern);
                 List<PharmacophoreQuery> defs = PharmacophoreUtils.readPharmacophoreDefinitions(sbis);
@@ -23,12 +58,14 @@ public class PSearch {
             } catch (CDKException e) {
                 System.out.println(e.getMessage());
             } catch (IOException e) {
-                System.out.println("Error in map setup: "+e.getMessage());
+                System.out.println("Error in map setup: " + e.getMessage());
 
             }
         }
 
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        public void map(LongWritable key, Text value,
+                        OutputCollector<Text, IntWritable> output,
+                        Reporter reporter) throws IOException {
             try {
                 StringReader sreader = new StringReader(value.toString());
                 MDLV2000Reader reader = new MDLV2000Reader(sreader);
@@ -38,32 +75,37 @@ public class PSearch {
 
                 boolean matched = pmatcher.matches(molecule);
                 matches.set((String) molecule.getProperty(CDKConstants.TITLE));
-                if (matched) context.write(matches, one);
-                else context.write(matches, zero);
+                if (matched) output.collect(matches, one);
+                else output.collect(matches, zero);
             } catch (CDKException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public static class PCoreMatchReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    public static class PCoreMatchReducer
+            extends MapReduceBase
+            implements Reducer<Text, IntWritable, Text, IntWritable> {
         private IntWritable result = new IntWritable();
 
-        public void reduce(Text key, Iterable<IntWritable> values,
-                           Context context) throws IOException, InterruptedException {
-            for (IntWritable val : values) {
-                if (val.compareTo(one) == 0) {
+
+        public void reduce(Text key, Iterator<IntWritable> values,
+                           OutputCollector<Text, IntWritable> output,
+                           Reporter reporter) throws IOException {
+            while (values.hasNext()) {
+                if (values.next().get() == 0) {
                     result.set(1);
-                    context.write(key, result);
+                    output.collect(key, result);
                 }
             }
         }
     }
 
     public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
+        Configuration configuration = new Configuration();
+        JobConf conf = new JobConf(configuration, PSearch.class);
 
-//        conf.set("mapred.job.tracker", "local");
+        conf.set("mapred.job.tracker", "local");
 
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
         if (otherArgs.length != 3) {
@@ -85,19 +127,18 @@ public class PSearch {
         // need to set it before we create the Job object
         conf.set("net.rguha.dc.data.pcoredef", sb.toString());
 
-        Job job = new Job(conf, "id 1");
-        job.setJarByClass(PSearch.class);
-        job.setMapperClass(MoleculeMapper.class);
-        job.setCombinerClass(PCoreMatchReducer.class);
-        job.setReducerClass(PCoreMatchReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        job.setInputFormatClass(SDFInputFormat.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
-        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
 
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        conf.setMapperClass(MoleculeMapper.class);
+        conf.setCombinerClass(PCoreMatchReducer.class);
+        conf.setReducerClass(PCoreMatchReducer.class);
+        conf.setInputFormat(SDFInputFormat.class);
+
+        FileInputFormat.setInputPaths(conf, args[0]);
+        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+        JobClient.runJob(conf);
+        System.exit(0);
     }
-    */
 }
