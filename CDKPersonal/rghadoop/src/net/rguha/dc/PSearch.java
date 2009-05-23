8634d2a2f0ed5d680c76bdb000e5f879cc1e43ba
Rajarshi  Guha <rajarshi.guha@gmail.com>
2009-05-22 23:01:20 -0400

    Added some authorship verbiage and license

--- CDKPersonal/rghadoop/src/net/rguha/dc/PSearch.java
+++ CDKPersonal/rghadoop/src/net/rguha/dc/PSearch.java
@@ -1,3 +1,23 @@
+/*
+ *
+ * Copyright (C) 2009 Rajarshi Guha <rajarshi.guha@gmail.com>
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package net.rguha.dc;
 
 import net.rguha.dc.io.SDFInputFormat;
@@ -115,6 +135,7 @@ public class PSearch extends Configured implements Tool {
     public int run(String[] args) throws Exception {
         if (args.length != 3) {
             System.err.println("Usage: psearch <in> <out> <pcorefile>");
+            System.err.println("Rajarshi Guha <rajarshi.guha@gmail.com>");
             System.exit(2);
         }
 
