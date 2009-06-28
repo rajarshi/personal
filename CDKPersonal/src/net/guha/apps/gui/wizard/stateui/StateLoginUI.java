471e097d61c7581d2c8b4bac611f400b3b2ac2eb
Rajarshi  Guha <rajarshi.guha@gmail.com>
2009-06-27 23:43:25 -0400

    Converted password field to a proper password field. Just have to make the connection. No error handling yet

--- CDKPersonal/src/net/guha/apps/gui/wizard/stateui/StateLoginUI.java
+++ CDKPersonal/src/net/guha/apps/gui/wizard/stateui/StateLoginUI.java
@@ -5,7 +5,7 @@ import com.jgoodies.forms.builder.DefaultFormBuilder;
 import com.jgoodies.forms.layout.FormLayout;
 
 import javax.swing.*;
-import java.util.ArrayList;
+import java.sql.Connection;
 
 /**
  * @author Rajarshi Guha
@@ -18,12 +18,23 @@ public class StateLoginUI extends WizardStateUI {
     private JTextField passwordField;
     private JTextField sidField;
 
+    private Connection conn;
+
     private void initComponents() {
         hostNameField = new JTextField();
         portField = new JTextField();
         userNameField = new JTextField();
-        passwordField = new JTextField();
+        passwordField = new JPasswordField();
         sidField = new JTextField();
+
+        // we set some default values for now
+        // should read these from a config file
+        hostNameField.setText("ncgcprobe.nhgri.nih.gov");
+        userNameField.setText("rnai");
+        passwordField.setText("ncgc");
+        sidField.setText("probedb");
+        portField.setText("1521");
+
     }
 
     public StateLoginUI() {
@@ -53,12 +64,39 @@ public class StateLoginUI extends WizardStateUI {
 
         builder.append("SID", sidField);
         builder.nextLine();
-        
+
         panel = builder.getPanel();
     }
 
 
     public Object evaluate() {
-        return new ArrayList();
+        String portString = portField.getText();
+        String hostname = hostNameField.getText();
+        String username = userNameField.getText();
+        String password = passwordField.getText();
+        String sid = sidField.getText();
+
+        if (portString == null ||
+                hostname == null ||
+                username == null ||
+                password == null ||
+                sid == null) return null;
+
+
+        System.out.println("password = " + password);
+        int port = 0;
+        try {
+            port = Integer.parseInt(portString);
+        } catch (NumberFormatException e) {
+            return null; // TODO should issue an error dialog
+        }
+
+        // make the connection
+
+        return new Object();
+    }
+
+    public Connection getConnection() {
+        return conn;
     }
 }
