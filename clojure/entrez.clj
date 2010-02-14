75e4f64b3823b29ed9aad495aaff52a141d7fa67
Rajarshi Guha <rajarshi.guha@gmail.com>
2010-02-14 10:37:26 -0500

    Updated esearch method to allow user to optionally specify the max and min dates via default keyword args

--- clojure/entrez.clj
+++ clojure/entrez.clj
@@ -11,13 +11,20 @@
 (defn get-ids [zipper]
   (zf/xml-> zipper :IdList :Id zf/text))
 
+(defn proc-date 
+  "If argument is a string return a CGI query term, otherwise empty string"
+  [name value]
+  (if (= 0 (count value))
+    (str "")
+    (str name "=" value "&")))
+
 ;; The :keys is a shortcut, to make the keys of the map the names of the
 ;; local variable. The :or key provides default values. We provide a 
 ;; pre-condition to ensure that we have a term to search on
 (defn esearch 
   "Perform an Entrez search. By default the database searched in PubMed. By default returns 1000000 ids that can be used in efetch. The :term keyword must be specified. All others are optional"
   [{:keys [term db retmode retmax mindate maxdate],
-    :or {db "pubmed", retmode "xml", retmax 1000000}}]
+    :or {db "pubmed", retmode "xml", mindate "", maxdate "", retmax 1000000}}]
   {:pre [(not (nil? term))]}
   (get-ids (parse-str (ds/slurp* 
 		       (str "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?"
@@ -25,8 +32,8 @@
 			    "term=" term "&"
 			    "retmode=" retmode "&"
 			    "retmax=" retmax "&"
-			    "mindate=" mindate "&"
-			    "maxdate=" maxdate "&"
+			    (proc-date "mindate" mindate)
+			    (proc-date "maxdate" maxdate)
 			    "email=rajarshi.guha@gmail.com")))))
 
   
@@ -38,3 +45,5 @@
  
 
 (count (esearch {:term "cheminformatics" :mindate "2009" :maxdate "2010"}))
+(count (esearch {:term "cheminformatics" }))
+
