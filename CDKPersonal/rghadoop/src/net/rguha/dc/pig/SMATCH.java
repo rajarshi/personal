6c5aa01883242fa176236830f08c85fc84c1799b
Rajarshi  Guha <rajarshi.guha@gmail.com>
2010-09-09 20:44:15 -0400

    Implemented a filter function to perform SMARTS matching

--- CDKPersonal/rghadoop/src/net/rguha/dc/pig/SMATCH.java
+++ CDKPersonal/rghadoop/src/net/rguha/dc/pig/SMATCH.java
@@ -2,13 +2,23 @@ package net.rguha.dc.pig;
 
 import org.apache.pig.FilterFunc;
 import org.apache.pig.data.Tuple;
-import org.openscience.cdk.smiles.smarts.SMARTSQueryTool;
+import org.apache.pig.impl.util.WrappedIOException;
+import org.openscience.cdk.DefaultChemObjectBuilder;
 import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.smiles.SmilesParser;
+import org.openscience.cdk.smiles.smarts.SMARTSQueryTool;
 
 import java.io.IOException;
 
 /**
  * Simple UDF to perform a SMARTS match.
+ *
+ * Example usage would be
+ * <code>
+ * A = load 'molecules.smi' as (smiles:chararray);
+ * B = filter A by net.rguha.dc.pig.SMATCH(smiles, 'NC(=O)C(=O)N');
+ * </code>
  *
  * @author Rajarshi Guha
  */
@@ -20,10 +30,18 @@ public class SMATCH extends FilterFunc {
             System.out.println(e);
         }
     }
+    static SmilesParser sp = new SmilesParser(DefaultChemObjectBuilder.getInstance());
 
     public Boolean exec(Tuple tuple) throws IOException {
-        if (tuple == null || tuple.size() == 0) return false;
-        
-        return false;
+        if (tuple == null || tuple.size() < 2) return false;
+        String target = (String) tuple.get(0);
+        String query = (String) tuple.get(1);
+        try {
+            sqt.setSmarts(query);
+            IAtomContainer mol = sp.parseSmiles(target);
+            return sqt.matches(mol);
+        } catch (CDKException e) {
+            throw WrappedIOException.wrap("Error in SMARTS pattern or SMILES string "+query, e);
+        }
     }
 }
